814e46acfbd4e02775f3322878c518a48cb4d5bf
Octavio-AAA <octavio.aaarau@gmail.com>
2021-11-13 23:07:59 -0300

    Foi ajustado o Blueprint 'auth' para nova tabela voluntario

--- blueprints/auth/auth.py
+++ blueprints/auth/auth.py
@@ -8,32 +8,32 @@ from imu.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
+#Foi definido essa rota para cadastro do refugiado.
 @bp.route('/register/', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        email = request.form['username']
+        senha = request.form['password']
         db = get_db()
         error = None
 
-        if  not username and not password:
+        if  not email and not senha:
             error = 'Insira Usuário e Senha!'
-        elif not username:
+        elif not email:
             error = 'Insira Usuário.'
-        elif not password:
+        elif not senha:
             error = 'Insira Senha.'        
 
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO refugiado (email, senha) VALUES (?, ?)",
+                    (email, generate_password_hash(senha)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"Usuario {username} já esta registrado."
+                error = f"Usuario {email} já esta registrado."
             else:
                 return redirect(url_for("auth.login"))
 
@@ -41,21 +41,54 @@ def register():
 
     return render_template('register.html')
 
+#Foi definido essa rota para cadastro do voluntário.
+@bp.route('/register_voluntario/', methods=('GET', 'POST'))
+def register_voluntario():
+    if request.method == 'POST':
+        email = request.form['username']
+        senha = request.form['password']
+        db = get_db()
+        error = None
 
+        if  not email and not senha:
+            error = 'Insira Usuário e Senha!'
+        elif not email:
+            error = 'Insira Usuário.'
+        elif not senha:
+            error = 'Insira Senha.'        
+
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO voluntario (email, senha) VALUES (?, ?)",
+                    (email, generate_password_hash(senha)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Usuario {email} já esta registrado."
+            else:
+                return redirect(url_for("auth.login_voluntario"))
+
+        flash(error)
+
+    return render_template('register.html')
+
+#Foi definido essa rota de login para refugiado.
 @bp.route('/login/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        email = request.form['username']
+        senha = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM refugiado WHERE email = ?', (email,)
         ).fetchone()
 
         if user is None:
             error = 'Usarname Incorreto.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user['senha'], senha):
             error = 'Senha Incorreto.'
 
         if error is None:
@@ -67,6 +100,31 @@ def login():
 
     return render_template('login.html')
 
+#Foi definido essa rota de login para voluntario.
+@bp.route('/login_voluntario/', methods=('GET', 'POST'))
+def login_voluntario():
+    if request.method == 'POST':
+        email = request.form['username']
+        senha = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM voluntario WHERE email = ?', (email,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Usarname Incorreto.'
+        elif not check_password_hash(user['senha'], senha):
+            error = 'Senha Incorreto.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('login_voluntario.html')
 
 @bp.route('/logout')
 def logout():
--- imu/schema.sql
+++ imu/schema.sql
@@ -1,6 +1,17 @@
 DROP TABLE IF EXISTS refugiado;
+DROP TABLE IF EXISTS voluntario;
 
 CREATE TABLE refugiado (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    nome TEXT NOT NULL,
+    sobrenome TEXT NOT NULL,
+    naturalidade TEXT NOT NULL,
+    senha TEXT NOT NULL
+);
+
+
+CREATE TABLE voluntario (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
     nome TEXT NOT NULL,
